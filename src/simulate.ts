66790958171c13c22997e9c075dd199db2c78288
NovanMondon <newworld01.bright@gmail.com>
2026-01-11 06:13:56 +0000

    コード整理

--- src/simulate.ts
+++ src/simulate.ts
@@ -1,4 +1,4 @@
-import { parseToCards, shuffleDeck } from "./simulator/card";
+import { parseToCards, shuffleDeck, type Card } from "./simulator/card";
 import { Game } from "./simulator/game";
 
 export const simulate = (
@@ -20,34 +20,44 @@ export const simulate = (
 
   output("Simulation Start");
 
-  const startTime = performance.now(); // 時間計測
-  type ResultRecord = { winner: 0 | 1, reason: string }
+  const startTime = performance.now();
+  const gameResultCount = simulateLoop([deck0.value, deck1.value], count);
+  const endTime = performance.now();
+  const elapsedTime = endTime - startTime;
+
+  // 結果の集計
+  const countWinners: Record<0 | 1, number> = { 0: 0, 1: 0 };
+  const countReasons: Record<string, number> = {};
+  for (const key of Object.keys(gameResultCount)) {
+    const result: ResultKey = JSON.parse(key);
+    countWinners[result.winner] = (countWinners[result.winner] ?? 0) + gameResultCount[key];
+    countReasons[result.reason] = (countReasons[result.reason] ?? 0) + gameResultCount[key];
+  }
+
+  output(`
+Winners: ${JSON.stringify(countWinners)} / ${count}
+Reasons: ${JSON.stringify(countReasons)} / ${count}
+Time:
+  Total ${elapsedTime} ms
+  ${elapsedTime / count * 1000000} ns/game
+
+Whole Result:\n${JSON.stringify(gameResultCount, null, 2)}
+    `.trim());
+};
+
+type ResultKey = { winner: 0 | 1, reason: string }
+
+function simulateLoop(decks: Card[][], count: number): Record<string, number> {
   const gameResultCount: Record<string, number> = {};
   for (let i = 0; i < count; i++) {
     // 先行は固定
-    const shuffledDecks = [shuffleDeck(deck0.value), shuffleDeck(deck1.value)];
+    const shuffledDecks = [shuffleDeck(decks[0]), shuffleDeck(decks[1])];
     const game = new Game(() => { });
     const result = game.simulateOnce(shuffledDecks);
     // 結果集計
     const key = JSON.stringify({ winner: result.winner, reason: result.reason });
     gameResultCount[key] = (gameResultCount[key] ?? 0) + 1;
   }
-  const endTime = performance.now(); // 時間計測
 
-  const countWinners: Record<0 | 1, number> = { 0: 0, 1: 0 };
-  const countReasons: Record<string, number> = {};
-  for (const key of Object.keys(gameResultCount)) {
-    const result: ResultRecord = JSON.parse(key);
-    countWinners[result.winner] = (countWinners[result.winner] ?? 0) + gameResultCount[key];
-    countReasons[result.reason] = (countReasons[result.reason] ?? 0) + gameResultCount[key];
-  }
-
-  output(`
-    Winners: ${JSON.stringify(countWinners)} / ${count}
-    Reasons: ${JSON.stringify(countReasons)} / ${count}
-    Time: Total ${(endTime - startTime)} ms
-      ${(endTime - startTime) / count * 1000000} ns/game
-
-    Whole Result:\n${JSON.stringify(gameResultCount, null, 2)}
-    `);
-};
+  return gameResultCount;
+}
